a99fb7004d60de6f19600a6ecb107399f9dc006d
Rigoni <davide.rigoni@tno.nl>
2024-05-29 09:39:34 +0200

    Finishing Recurrent PPO

--- CybORG/Agents/Recurrent_PPO/network.py
+++ CybORG/Agents/Recurrent_PPO/network.py
@@ -2,61 +2,89 @@ import torch
 import torch.nn as nn
 from torch.distributions import Categorical
 import torch.nn.functional as F
+import numpy as np
 
-# This actor-critic implementation follows the implementation of: https://gitlab.com/ngoodger/ppo_lstm/-/blob/master/recurrent_ppo.ipynb
 class Actor(nn.Module):
     def __init__(self, state_dim, action_dim):
         super().__init__()
-        self.hidden_size = 128
+        self.hidden_size = 256
         self.recurrent_layers = 1
         # Create 1 Layer of LSTM
-        self.lstm = nn.LSTM(state_dim, self.hidden_size, num_layers=self.recurrent_layers)
+        self.lstm = nn.LSTM(state_dim, self.hidden_size, batch_first=True)
+        for name, param in self.lstm.named_parameters():
+            if "bias" in name:
+                nn.init.constant_(param, 0)
+            elif "weight" in name:
+                nn.init.orthogonal_(param, 1.0)
+        # TODO: This can be of different sizes
         self.layer_hidden = nn.Linear(self.hidden_size, self.hidden_size)
-        self.layer_policy_logits = nn.Linear(self.hidden_size, action_dim)
-        self.action_dim = action_dim
-        self.hidden_cell = None
+        nn.init.orthogonal_(self.layer_hidden.weight, np.sqrt(2))
+        # Hidden policy layer
+        self.lin_policy = nn.Linear(self.hidden_size, self.hidden_size)
+        nn.init.orthogonal_(self.lin_policy.weight, np.sqrt(2))
+        # Final policy layer
+        self.policy_layer = nn.Linear(in_features=self.hidden_size, out_features=action_dim)
+        nn.init.orthogonal_(self.policy_layer.weight, np.sqrt(0.01))
+        self.recurrent_cell = None
     
     # Initial hidden state of size 1   
     def get_init_state(self, batch_size):
-        self.hidden_cell = (torch.zeros(self.recurrent_layers, batch_size, self.hidden_size),
-                            torch.zeros(self.recurrent_layers, batch_size,self.hidden_size))
+        self.recurrent_cell = (torch.zeros(batch_size, self.hidden_size).unsqueeze(0),
+                            torch.zeros(batch_size, self.hidden_size).unsqueeze(0))
         
-    def forward(self, state):
-        batch_size = state.shape[0] # Always = 1
-        if self.hidden_cell is None or batch_size != self.hidden_cell[0].shape[1]:
-            self.get_init_state(batch_size)
-        state = state.unsqueeze(0)
-        # Set new hidden state cell (send to lstm layer the old hidden cell)
-        _, self.hidden_cell = self.lstm(state, self.hidden_cell)
-        hidden_out = F.elu(self.layer_hidden(self.hidden_cell[0][-1]))
-        policy_logits_out = self.layer_policy_logits(hidden_out)
-        # Get policy distribution
-        policy_dist = Categorical(F.softmax(policy_logits_out, dim=1))
+    def forward(self, state, sequence_length = 1):
+        if sequence_length == 1:
+            state, self.recurrent_cell = self.lstm(state.unsqueeze(1), self.recurrent_cell)
+            state = state.squeeze(1)
+        else:
+            state, self.recurrent_cell = self.lstm(state, self.recurrent_cell)
+            state_shape = tuple(state.size())
+            state = state.reshape(state_shape[0]*state_shape[1], state_shape[2])
+
+        state = F.relu(self.layer_hidden(state))
+        policy_head = F.relu(self.lin_policy(state))
+        policy_dist = Categorical(logits=self.policy_layer(policy_head))
         return policy_dist
     
 class Critic(nn.Module):
     def __init__(self, state_dim):
         super().__init__()
-        self.hidden_size = 128
+        self.hidden_size = 256
         self.recurrent_layers = 1
-        self.layer_lstm = nn.LSTM(state_dim, self.hidden_size, num_layers=self.recurrent_layers)
+        # Create 1 Layer of LSTM
+        self.lstm = nn.LSTM(state_dim, self.hidden_size, batch_first=True)
+        for name, param in self.lstm.named_parameters():
+            if "bias" in name:
+                nn.init.constant_(param, 0)
+            elif "weight" in name:
+                nn.init.orthogonal_(param, 1.0)
+        # TODO: This can be of different sizes
         self.layer_hidden = nn.Linear(self.hidden_size, self.hidden_size)
-        self.layer_value = nn.Linear(self.hidden_size, 1)
-        self.hidden_cell = None
+        nn.init.orthogonal_(self.layer_hidden.weight, np.sqrt(2))
+        # Hidden policy layer
+        self.lin_value = nn.Linear(self.hidden_size, self.hidden_size)
+        nn.init.orthogonal_(self.lin_value.weight, np.sqrt(2))
+        # Value layer
+        self.value_layer = nn.Linear(self.hidden_size, 1)
+        nn.init.orthogonal_(self.value_layer.weight, 1)
+        self.recurrent_cell = None
     
     # Initial hidden state of size 1 
     def get_init_state(self, batch_size):
-        self.hidden_cell = (torch.zeros(self.recurrent_layers, batch_size, self.hidden_size),
-                            torch.zeros(self.recurrent_layers, batch_size, self.hidden_size))
+        self.recurrent_cell = (torch.zeros(batch_size, self.hidden_size).unsqueeze(0),
+                            torch.zeros(batch_size, self.hidden_size).unsqueeze(0))
     
-    def forward(self, state):
-        batch_size = state.shape[0]
-        if self.hidden_cell is None or batch_size != self.hidden_cell[0].shape[1]:
-            self.get_init_state(batch_size)
-        state = state.unsqueeze(0)
-        # Set new hidden state cell (send to lstm layer the old hidden cell)
-        _, self.hidden_cell = self.layer_lstm(state, self.hidden_cell)
-        hidden_out = F.elu(self.layer_hidden(self.hidden_cell[0][-1]))
-        # Output the value of the state
-        value_out = self.layer_value(hidden_out)
-        return value_out#.detach()
+    def forward(self, state, sequence_length = 1):
+        if sequence_length == 1:
+            state, self.recurrent_cell = self.lstm(state.unsqueeze(1), self.recurrent_cell)
+            state = state.squeeze(1)
+        else:
+            state, self.recurrent_cell = self.lstm(state, self.recurrent_cell)
+            # Don't resave model
+            # state, _ = self.lstm(state, self.recurrent_cell)
+            state_shape = tuple(state.size())
+            state = state.reshape(state_shape[0]*state_shape[1], state_shape[2])
+        state = F.relu(self.layer_hidden(state))
+        hidden_value = F.relu(self.lin_value(state))
+        value = self.value_layer(hidden_value).reshape(-1)
+        return value
---
+++ r_ppo_trainer.py
@@ -0,0 +1,118 @@
+from CybORG import CybORG
+from CybORG.Simulator.Scenarios import EnterpriseScenarioGenerator
+from CybORG.Agents.Wrappers import BlueFlatWrapper
+from CybORG.Agents.Recurrent_PPO.ppo2 import PPO
+from CybORG.Agents import SleepAgent, EnterpriseGreenAgent, FiniteStateRedAgent
+from statistics import mean, stdev
+
+import csv
+import matplotlib.pyplot as plt
+
+
+EPISODE_LENGTH = 500
+MAX_EPS = 4000
+LOAD_NETWORKS = False
+LOAD_BEST = False
+ROLLOUT = 10
+
+def main():
+    # Initialize CybORG environment
+    sg = EnterpriseScenarioGenerator(blue_agent_class=SleepAgent,
+                                     green_agent_class=EnterpriseGreenAgent,
+                                     red_agent_class=FiniteStateRedAgent,
+                                     steps=EPISODE_LENGTH)
+    cyborg = CybORG(scenario_generator=sg, seed=1) # Add Seed
+    env = BlueFlatWrapper(env=cyborg)
+    env.reset()
+    # TODO: Check for 'Labels' and 'Mask' in the action space
+    agents = {f"blue_agent_{agent}": PPO(env.observation_space(f'blue_agent_{agent}').shape[0], len(env.get_action_space(f'blue_agent_{agent}')['actions']), MAX_EPS*EPISODE_LENGTH, agent) for agent in range(5)}
+    print(f'Using agents {agents}')
+    if LOAD_NETWORKS:
+        for agent_name, agent in agents.items():
+            if LOAD_BEST:
+                agent.load_network()
+            else:
+                agent.load_last_epoch()
+    # TODO: Add recording of time
+    total_rewards = [] 
+    count = 0 # Keep track of total episodes
+    partial_rewards = 0
+    reward_before_update = 0
+    best_reward = -2000
+    average_rewards = []
+    for i in range(MAX_EPS):
+        # Reset the environment for each training episode
+        observations, _ = env.reset()
+        for agent_name, agent in agents.items():
+            agent.set_initial_state(1)
+        r = []
+        for j in range(EPISODE_LENGTH): # Episode length
+            count += 1
+            for agent_name, agent in agents.items():
+                agent.save_lstm_state()
+            # Action selection for all agents
+            actions = {
+                agent_name: agent.get_action(
+                    observations[agent_name]
+                )
+                for agent_name, agent in agents.items() 
+                if agent_name in env.agents
+            }
+            # Perform action on the environment
+            observations, reward, termination, truncation, _ = env.step(actions)
+
+            # Append the rewards and termination for each agent
+            for agent_name, agent in agents.items():
+                done = termination[agent_name] or truncation[agent_name]
+                agent.episodic_rewards.append(reward[agent_name]) # Save reward
+                agent.episodic_termination.append(done) # Save termination
+            # This terminates if all agent have 'termination=true'
+            done = {
+                agent: termination.get(agent, False) or truncation.get(agent, False)
+                for agent in env.agents
+            }
+            # If all agents are done (truncation) then end the episode
+            if all(done.values()):
+                break
+            r.append(mean(reward.values())) # Add rewards  
+        # Add to partial rewards  
+        partial_rewards += sum(r)
+        reward_before_update += sum(r)
+        print(f"Final reward of the episode: {sum(r)}, length {count} - AVG: {partial_rewards/(i+1)}")
+        total_rewards.append(sum(r)) 
+        if (i+1) % ROLLOUT == 0:
+            average_rewards.append(reward_before_update/ROLLOUT)
+            if reward_before_update/ROLLOUT > best_reward:
+                best_reward = reward_before_update/ROLLOUT
+                for agent_name, agent in agents.items():
+                    agent.save_network()
+            reward_before_update = 0
+        # Save rewards, state values and termination flags (divided per episodes)    
+        for agent_name, agent in agents.items():
+            agent.append_episodic()
+            agent.clear_episodic()
+            # Learn at every episode
+            if (i+1) % ROLLOUT == 0:
+                print(f"Policy update for  {agent_name}. Total steps: {count}")
+                agent.learn(count) 
+    # Graph of average rewards and print output results 
+    for agent_name, agent in agents.items():
+        agent.save_statistics_csv() 
+        agent.save_last_epoch()
+    rewards_mean = mean(total_rewards)
+    rewards_stdev = stdev(total_rewards)
+    total_rewards_transposed = [[elem] for elem in average_rewards]
+    with open('reward_history.csv', mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['Rewards'])  # Write header
+        writer.writerows(total_rewards_transposed)
+    plt.plot(total_rewards)
+    plt.xlabel('Episode')
+    plt.ylabel('Reward')
+    plt.title('Reward per Episode')
+    plt.grid(True)
+    plt.show()
+    print(f"Average reward: {rewards_mean}, standard deviation of {rewards_stdev}")
+
+if __name__ == "__main__":
+    main()
