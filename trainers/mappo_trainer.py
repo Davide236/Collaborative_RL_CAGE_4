09980ecb712149b3cf853c36e2a8e33ba96e762d
Rigoni <davide.rigoni@tno.nl>
2024-11-09 13:06:08 +0100

    MAPPO training

--- trainers/mappo_trainer.py
+++ trainers/mappo_trainer.py
@@ -9,7 +9,7 @@ import numpy as np
 import torch
 import os
 import yaml
-from utils import save_statistics, save_agent_data_ppo, save_agent_network, rewards_handler
+from utils import save_statistics, save_agent_data_ppo, save_agent_network, rewards_handler, RewardNormalizer
 
 class MAPPOTrainer:
     EPISODE_LENGTH = 500
@@ -90,6 +90,7 @@ class MAPPOTrainer:
 
     def run(self):
         self.initialize_environment()
+        reward_normalizer = RewardNormalizer()
         for i in range(self.MAX_EPS):
             # Reset the environment for each training episode
             observations, _ = self.env.reset()
@@ -120,7 +121,7 @@ class MAPPOTrainer:
                 # Append the rewards and termination for each agent
                 for agent_name, agent in self.agents.items():
                     done = termination[agent_name] or truncation[agent_name]
-                    agent.memory.save_end_episode(reward[agent_name], done, observations_list)
+                    agent.memory.save_end_episode(reward_normalizer.normalize(reward[agent_name]), done, observations_list)
                 # This terminates if all agents have 'termination=true'
                 done = {
                     agent: termination.get(agent, False) or truncation.get(agent, False)
@@ -128,7 +129,7 @@ class MAPPOTrainer:
                 }
                 if all(done.values()):
                     break
-                r.append(mean(reward.values()))  # Add rewards
+                r.append(sum(reward.values()))  # Add rewards
             self.partial_rewards += sum(r)
             self.total_rewards.append(sum(r))
             print(f"Final reward of the episode: {sum(r)}, length {self.count} - AVG: {mean(self.total_rewards)}")
